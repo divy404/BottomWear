035861e49abf63e4b64c3bc41e9ec638ff2867f0
divy404 <divyanshj360@gmail.com>
2026-01-23 22:07:19 +0530

    updateCart

--- Backend/src/controllers/cartController.js
+++ Backend/src/controllers/cartController.js
@@ -1,3 +1,4 @@
+import e from "express";
 import Cart from "../models/Cart.js";
 import Product from "../models/Product.js";
 
@@ -90,4 +91,69 @@ export const getCart = async (req,res) => {
            message: "Server error",
          });
     }
+}
+
+export const updateCartItem = async (req,res) => {
+  try {
+    const userId = req.user.id;
+    const { productId, quantity } = req.body;
+
+    const qty = Number(quantity);
+    if (!productId) {
+      return res.status(400).json({
+        message: "ProductId is required",
+      });
+    }
+
+    if (isNaN(qty)) {
+      return res.status(400).json({
+        message: "Quantity must be a number",
+      });
+    }
+
+    const cart = await Cart.findOne({ user: userId });
+
+    if (!cart) {
+      return res.status(404).json({
+        message: "Cart not found",
+      });
+    }
+
+    const itemIndex = cart.items.findIndex(
+      (item) => item.product.toString() === productId,
+    );
+
+    if (itemIndex === -1) {
+      return res.json(404).json({
+        message: "Product not found in cart",
+      });
+    }
+
+    if (qty === 0) {
+      cart.items.splice(itemIndex, 1);
+    } else if (qty < 0) {
+      return res.status(400).json({
+        message: "Quantity cannot be negative",
+      });
+    } else {
+      // Set exact quantity
+      cart.items[itemIndex].quantity = qty;
+    }
+
+    // Recalculate total price
+    cart.totalPrice = cart.items.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0,
+    );
+
+    await cart.save();
+
+    res.json(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+    
+  }
 }
--- Backend/src/routes/cartRoutes.js
+++ Backend/src/routes/cartRoutes.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { addToCart,getCart } from "../controllers/cartController.js"
+import { addToCart,getCart,updateCartItem } from "../controllers/cartController.js"
 import {protect} from "../middleware/authMiddleware.js"
 
 const router = express.Router();
 
-router.post("/",protect,addToCart);
-router.get("/",protect,getCart);
+router.post("/",protect,addToCart); // to add from product page in cart
+router.get("/",protect,getCart); 
+router.put("/",protect,updateCartItem); // update _,- inside cart
+
 
 export default router;
