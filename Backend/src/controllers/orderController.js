3be0d4496188df5a2f25d8ce77e1da4298846ca0
divy404 <divyanshj360@gmail.com>
2026-02-03 23:57:08 +0530

    updateorderStatus

--- Backend/src/controllers/orderController.js
+++ Backend/src/controllers/orderController.js
@@ -1,95 +1,136 @@
 import mongoose from "mongoose";
 import Cart from "../models/Cart.js";
 import Order from "../models/Order.js";
+import { response } from "express";
 
-export const placeOrder = async (req,res) => {
-    try {
-        const userId = req.user.id;
-        const {paymentMethod} = req.body;
+export const placeOrder = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { paymentMethod } = req.body;
 
-        const cart = await Cart.findOne({user:userId});
-        
-        if(!cart || cart.items.length===0) {
-            return res.status(400).json({
-                message:"Cart is empty"
-            })
-        }
+    const cart = await Cart.findOne({ user: userId });
 
-        const orderItems = cart.items.map(item => ({
-            product: item.product,
-            quantity: item.quantity,
-            price: item.price,
-        }));
-
-        const order = await Order.create({
-            user: userId,
-            items: orderItems,
-            totalAmount: cart.totalPrice,
-            paymentMethod: paymentMethod || "COD",
-            paymentStatus: paymentMethod === "ONLINE" ? "PAID" : "PENDING"
-        });
-
-        //clear cart after order
-        cart.items = [];
-        cart.totalPrice = 0;
-        await cart.save();
-
-        res.status(201).json(order);
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message:"Server error"
-        })
-        
+    if (!cart || cart.items.length === 0) {
+      return res.status(400).json({
+        message: "Cart is empty",
+      });
     }
-}
 
-export const getUserOrders = async (req,res) => {
-    try {
-        const userId = req.user.id;
+    const orderItems = cart.items.map((item) => ({
+      product: item.product,
+      quantity: item.quantity,
+      price: item.price,
+    }));
 
-        const orders  = await Order.find({user:userId}).sort({createdAt:-1});
-        // this makes sure new order appears at top
+    const order = await Order.create({
+      user: userId,
+      items: orderItems,
+      totalAmount: cart.totalPrice,
+      paymentMethod: paymentMethod || "COD",
+      paymentStatus: paymentMethod === "ONLINE" ? "PAID" : "PENDING",
+    });
 
-        res.json(orders);
+    //clear cart after order
+    cart.items = [];
+    cart.totalPrice = 0;
+    await cart.save();
 
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message:"Internal server error"
-        })
-        
+    res.status(201).json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export const getUserOrders = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const orders = await Order.find({ user: userId }).sort({ createdAt: -1 });
+    // this makes sure new order appears at top
+
+    res.json(orders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getOrderById = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const userId = req.user.id;
+
+    if (!mongoose.Types.ObjectId.isValid(orderId)) {
+      return res.json(400).json({
+        message: "Invalid order ID",
+      });
     }
-}
 
-export const getOrderById = async (req,res) => {
-    try {
-        const {orderId} = req.params;
-        const userId = req.user.id;
+    const order = await Order.findOne({
+      _id: orderId,
+      user: userId,
+    }).populate("items.product", "name price images");
 
-        if(!mongoose.Types.ObjectId.isValid(orderId)) {
-            return res.json(400).json({
-                message:"Invalid order ID"
-            })
-        }
-
-        const order = await Order.findOne({
-            _id: orderId,
-            user: userId
-        }).populate("items.product", "name price images");
-
-        if(!order) {
-            return res.status(404).json({
-                message:"Order not found"
-            })
-        }
-        res.json(order);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            message:"Server error"
-        })
-        
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
     }
-}
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
+
+export const updateOrderStatus = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const allowedStatuses = ["PLACED", "SHIPPED", "DELIVERED", "CANCELLED"];
+
+    if (!allowedStatuses.includes(status)) {
+      return res.status(400).json({
+        message: "Invalid order status",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(orderId)) {
+      return res.status(400).json({
+        message: "Invalid order ID",
+      });
+    }
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        message: "Order not found",
+      });
+    }
+
+    order.orderStatus = status;
+
+    // optional: auto-set payment status
+    if (status === "DELIVERED" && order.paymentMethod === "COD") {
+      order.paymentStatus = "PAID";
+    }
+
+    await order.save();
+
+    res.json(order);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Server error",
+    });
+  }
+};
--- Backend/src/routes/orderRoutes.js
+++ Backend/src/routes/orderRoutes.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { placeOrder,getUserOrders,getOrderById } from "../controllers/orderController.js";
+import { placeOrder,getUserOrders,getOrderById,updateOrderStatus } from "../controllers/orderController.js";
 import {protect} from "../middleware/authMiddleware.js";
+import {adminOnly} from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 router.post("/",protect,placeOrder);
 router.get("/",protect,getUserOrders);
 router.get("/:orderId",protect,getOrderById);
+router.put("/:orderId/status", protect, adminOnly, updateOrderStatus);
+
 
 export default router;
